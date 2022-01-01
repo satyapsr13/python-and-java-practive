82c9760c66af68ba9148f9c2aef024f8408e9a94
satyaprakashsinghrathour <satyapsr13@gmail.com>
2022-01-01 18:16:31 -0500

    ..

--- firstDay.py
+++ firstDay.py
@@ -4,3 +4,11 @@ a = b = c = 10
 s = "i am sasfjdsndfldjs"
 print(a+b, s)
 # print(type(a))
+if a > b:
+    print("a>b")
+else:
+    print("less than")
+    for a in range(1, 34):
+        print(a+print(a))
+
+        print(a)
